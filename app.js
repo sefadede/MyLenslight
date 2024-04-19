53e42c46ea4772ffcf0d0dd9e78a1f505f346e47
sefadede <sefa_dede@yahoo.com.tr>
2024-04-19 21:30:49 +0300

    db coonnection

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
-import express from "express"
+import express from "express";
+import dotenv from "dotenv"; 
+import conn from "./db.js";
+
+dotenv.config();
+
+//Connection to the DB
+conn();
 
 const app = express();
-const port = 3000
+const port = process.env.PORT;
+
+//Ejs template engine
+app.set("view engine","ejs");
+
+//static files middleware
+app.use(express.static('public'))
 
 app.get("/",(req,res)=>{
-    res.send("Index Sayfası1")
+   res.render('index');
 });
+
+app.get("/about",(req,res)=>{
+    res.render('about');
+ });
 
 app.listen(port,()=>{
     console.log(`Application running on port: ${port}`)
